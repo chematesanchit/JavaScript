0662c5e147ae1303b58af775b07608b181073210
sanchit <sanchitchemate@gmail.com>
2024-01-17 16:46:20 +0530

    dataTypes

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"
+// Treat all JS code as newer version
+// alert(3 + 3); // We are using Node.js, not a browser
+console.log(3 + 3)
+// Code readability should be high
+console.log("Sanchit")
+
+
+/// dataType////
+
+let age = 18;
+let isLoggedIn = false;
+let state; // Declare a variable without initializing it
+
+let bigInt = BigInt(1234567890123456789012345678901234567890n); // Example of a BigInt
+
+let myString = "Hello, World!"; // Example of a string
+
+let isBoolean = true; // Example of a boolean (true/false)
+
+let nullValue = null; // Example of a null standalone value
+
+let undefinedValue; // Example of an undefined variable
+
+
+// Symbol: unique
+let mySymbol = Symbol("unique");
+
+// Object
+
+
+;
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object
+
