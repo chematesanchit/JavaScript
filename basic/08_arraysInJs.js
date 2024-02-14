0d49601964f28185637978151f54702e2a198a7c
sanchit <sanchitchemate@gmail.com>
2024-02-14 21:26:38 +0530

    arrays in js

---
+++ basic/08_arraysInJs.js
@@ -0,0 +1,21 @@
+const arry =["sanchit",1,3,5.6]
+const arry2=new Array("chemate",4,66,66.666,34)
+//console.log(arry,arry2)
+
+// Array methods
+
+arry.push(7)
+arry.push(8)
+arry.push(6)
+arry.push(45)
+arry.pop()
+arry.unshift("sanchit")
+//console.log(arry)
+
+console.log("A  ", arry)
+const array3=arry.slice(2,6)
+console.log(array3)
+console.log("B ",arry)
+const array4=arry.splice(2,6)
+console.log(array3)
+console.log("c ",arry)
